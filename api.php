102a0e5ed2281b1d6480665b83a3fd91263dd129
Camille Scholtz <camille@airmail.cc>
2017-09-15 18:11:38 +0200

    Add ID, add "view all upload files" option

--- api.php
+++ api.php
@@ -11,13 +11,29 @@ define(URL, "https://punpun.xyz/");
 // a trailing slash.
 define(DIR, "/srv/punpun.xyz/uploads/");
 
-// The seekrit key.
-define(KEY, "seekrit");
-
+// IDs and their respective keys.
+// TODO: I actually want to define() this as well for consistency.
+$IDS = [
+	"id1" => "pass",
+	"id2" => "pass",
+];
 
 //
 // Functions.
 //
+
+// check_format checks if the request is a POST request, and the
+// desired output format. This function can return FALSE, "plain" or
+// "html".
+function check_format() {
+	if ($_SERVER["REQUEST_METHOD"] != "POST") {
+		return FALSE;
+	}
+
+	// Check if we shoud return pretty HTML output or plain output
+	// (for something like cURL).
+	return isset($_GET["output"]) ? $_GET["output"] : "plain";
+}
 
 // random_string generates a random string of a given lenght.
 function random_string($lenght) {
@@ -31,19 +47,43 @@ function random_string($lenght) {
 	return $string;
 }
 
-// check_method checks if the request is a POST request, and the
-// desired output. This function can return FALSE, "plain" or "html".
-function check_method() {
-	if ($_SERVER["REQUEST_METHOD"] != "POST") {
-		return FALSE;
+// generate_name generates a random, non-existing file name.
+function generate_name($orig_name) {
+	// Generate random name.
+	$new_name = random_string(4) . "." . end((explode(".",
+		$orig_name)));
+
+	// Check if the file alread exists.
+	foreach ($IDS as $id) {
+		if (file_exists(DIR . $id . "/" . $new_name)) {
+			return generate_name($orig_name);
+		}
 	}
 
-	// Check if we shoud return pretty html output or plain output
-	// (for something like cURL).
-	return isset($_GET["output"]) ? $_GET["output"] : "plain";
+	return $new_name;
 }
 
-// upload_files uploads files and returns a list with URLs of the
+// view_files returns an array with URLs of all files uploaded by a
+// specific ID.
+function view_files() {
+	$urls = [];
+
+	foreach (scandir(DIR . $_POST["id"]) as $file) {
+		// Exclude hidden files.
+		if ($file[0] == '.') {
+			continue;
+		}
+
+		$urls[$file] = filemtime(DIR . $_POST["id"] . "/" . $file);
+	}
+
+	asort($urls);
+	$urls = array_reverse(array_keys($urls));
+
+	return $urls;
+}
+
+// upload_files uploads files and returns an array with URLs of the
 // uploaded files.
 function upload_files() {
 	$urls = [];
@@ -51,15 +91,12 @@ function upload_files() {
 	foreach ($_FILES["files"]["error"] as $key => $error) {
 		if ($error == UPLOAD_ERR_OK) {
 			$orig_name = $_FILES["files"]["name"][$key];
-			$temp_name = $_FILES["files"]["tmp_name"][$key];
+			$tmp_name = $_FILES["files"]["tmp_name"][$key];
 
-			// Generate a non-existing random new name.
-			while (file_exists(DIR . $new_name)) {
-				$new_name = random_string(4) . "." . end((explode(".",
-					$orig_name)));
-			}
+			$new_name = generate_name($orig_name);
 
-			if (move_uploaded_file($temp_name, DIR . $new_name)) {
+			if (move_uploaded_file($tmp_name, DIR . $_POST["id"] .
+				"/" . $new_name)) {
 				$urls[] = URL . $new_name;
 			}
 		}
@@ -73,26 +110,30 @@ function upload_files() {
 // Main.
 //
 
-$valid_key = $_POST["key"] == KEY;
+$valid_key = $IDS[$_POST["id"]] == $_POST["key"];
+$format = check_format();
 
-$method = check_method();
-if ($method == "html") {
+if ($format == "html") {
 	include "header.html";
 	if ($valid_key) {
-		foreach (upload_files() as $url) {
+		$urls = isset($_POST["view"]) ? view_files() : upload_files();
+
+		foreach ($urls as $url) {
 			echo "			<li><a href=\"" . $url .
 				"\">$url</a></li>\n";
 		}
 	} else {
-		echo "<h3>Invalid key!</h3>\n";
+		echo "<h3>Invalid ID or key!</h3>\n";
 	}
 	include "footer.html";
-} elseif ($method == "plain") {
+} elseif ($format == "plain") {
 	if ($valid_key) {
-		foreach (upload_files() as $url) {
+		$urls = isset($_POST["view"]) ? view_files() : upload_files();
+
+		foreach ($urls as $url) {
 			echo $url . "\n";
 		}
 	} else {
-		echo "Invalid key!\n";
+		echo "Invalid ID or key!\n";
 	}
 }
--- index.md
+++ index.md
@@ -5,10 +5,13 @@ title = "UPLOAD"
 <body>
 	<form enctype="multipart/form-data" method="post" action="api.php?output=html">
 		<p><strong>File:</strong></p>
-		<input type="file" name="files[]" multiple required/>
-		<p><strong>Key:</strong></p>
+		<input type="file" name="files[]" multiple/>
+		<p><strong>ID:           Key:</strong></p>
+		<input type="text" name="id" required/>
 		<input type="password" name="key" required/>
-		<input type="submit" value="Upload..."/>
+		<input type="submit" name="upload" value="Upload..."/>
+		</br></br></br>
+		<input type="submit" name="view" value="View all uploads"/>
 	</form>
 </body>
 
@@ -21,9 +24,15 @@ You can also use `curl` to upload files:
 #!/bin/fish
 
 set f '/path/to/file'
+
+set i 'DarkLord66'
 set k 'seekrit'
 
-curl -F files[]="@$f" -F key="$k" https://punpun.xyz/upload/api.php
+# Upload files
+curl -F files[]="@$f" -F id="$i" -F key="$k" \
+	https://punpun.xyz/upload/api.php
+
+# View list of uploaded files
 ```
 
 Alternativly, use [punf](https://github.com/onodera-punpun/punf).
